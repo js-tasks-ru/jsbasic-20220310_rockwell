d7b3ff140fcf9c146e8f84f8cd78aef61b4b6adb
rockwell <sergey.kim_thedas@bk.ru>
2022-04-05 02:21:22 +0300

    Решение заданий модуля 4 и 5

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,27 @@
 function highlight(table) {
-  // ваш код...
+  for(let i = 1; i < table.rows.length; i++){
+    for( let n = 0; n < table.rows[i].cells.length; n++){
+        if (table.rows[i].cells[n].hasAttribute('data-available')){
+            if (table.rows[i].cells[n].getAttribute('data-available') == 'true'){
+                table.rows[i].cells[n].parentNode.classList.add('available');
+            } else{
+                table.rows[i].cells[n].parentNode.classList.add('unavailable');
+            }
+        }
+        if (table.rows[i].cells[n].innerHTML == 'm'){
+            table.rows[i].cells[n].parentNode.classList.add('male');
+        }
+        if (table.rows[i].cells[n].innerHTML == 'f'){
+            table.rows[i].cells[n].parentNode.classList.add('female');
+        }
+        if (isFinite(table.rows[i].cells[n].innerHTML)){
+            if (+table.rows[i].cells[n].innerHTML < 18){
+                table.rows[i].cells[n].parentNode.style = "text-decoration: line-through";
+            }
+        }
+        if (!table.rows[i].cells[n].parentNode.classList.contains('available') && !table.rows[i].cells[n].parentNode.classList.contains('unavailable') ){
+            table.rows[i].cells[n].parentNode.setAttribute('hidden', 'true');
+        }
+    }
+  }
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,46 @@
 function initCarousel() {
-  // ваш код...
+  let elem = document.querySelector('[data-carousel-holder]');
+  let slideNumber = 0;
+  let slideAmount = 4;
+  let carouselInner = document.querySelector('.carousel__inner');
+  let carouselArrowRight = document.querySelector('.carousel__arrow_right');
+  let carouselArrowLeft = document.querySelector('.carousel__arrow_left');
+
+  function right() {
+    slideNumber++;
+    update();
+  }
+
+  function left() {
+    slideNumber--;
+    update();
+  }
+
+  function update() {
+    let offset = carouselInner.offsetWidth * slideNumber;
+    carouselInner.style.transform = `translateX(-${offset}px)`;
+
+    if (slideNumber == slideAmount - 1){
+      carouselArrowRight.style.display = 'none';
+    } else{
+      carouselArrowRight.style.display ='';
+    }
+
+    if (slideNumber == 0){
+      carouselArrowLeft.style.display = 'none';
+    } else{
+      carouselArrowLeft.style.display = '';
+    }
+  }
+
+  update();
+
+  elem.onclick = ({target}) =>{
+    if (target.closest('.carousel__arrow_right')){
+      right();
+    }
+    if (target.closest('.carousel__arrow_left')){
+      left();
+    }
+  }
 }
